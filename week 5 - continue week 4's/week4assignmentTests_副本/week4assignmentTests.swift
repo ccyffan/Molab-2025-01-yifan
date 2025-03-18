fc6ef6b2cb26bab388bb5ae64c69df671e4094f8
yifan chen <yc4351@nyu.edu>
2025-03-17 23:55:46 -0400

    Add files via upload

---
+++ week 5 - continue week 4's/week4assignmentTests_副本/week4assignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  week4assignmentTests.swift
+//  week4assignmentTests
+//
+//  Created by 陈艺凡 on 2/25/25.
+//
+
+import Testing
+@testable import week4assignment
+
+struct week4assignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
